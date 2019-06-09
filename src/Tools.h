2fe9c60f61fde68fcbf2b043c7878340975d9b2f
Zlatin Georgiev <32363835+alphasoftwarebg@users.noreply.github.com>
2019-06-09 21:08:10 +0300

    Update Tools.h

--- src/Tools.h
+++ src/Tools.h
@@ -1,15 +1,14 @@
-																			 /*
+/*
 -------- Tools.h --------------------------------------------------------------
 
-	ZZZ Base Mini sample warehouse program
+ZZZ Base Mini sample warehouse program
 
 -------- © 2019 ZZZ Ltd. - Bulgaria -------------------------------------------
-																			 */
+*/
 #pragma once
 
 #include <string>
 
-
-std::string ToUtf8(const wchar_t * in);
+std::string ToUtf8(const wchar_t* in);
 double ToDouble(std::string const& numberString);
 std::string ToString(double number);
