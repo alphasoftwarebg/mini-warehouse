ed4a672292225c1d69a6cc4fd771e4cb67e00f42
Zlatin Georgiev <32363835+alphasoftwarebg@users.noreply.github.com>
2019-06-09 21:08:55 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,18 @@
-																			 /*
+/*
 -------- main.cpp -------------------------------------------------------------
 
-	ZZZ Base Mini sample warehouse program
+ZZZ Base Mini sample warehouse program
 
 -------- © 2019 ZZZ Ltd. - Bulgaria -------------------------------------------
-																			 */
+*/
 #ifdef _WIN32
-#	include <windows.h>
-#	include <tchar.h>
+#include <tchar.h>
+#include <windows.h>
 #endif
 #include "Store.h"
 #include "Tools.h"
 
-
 using namespace std;
-
 
 #ifdef _WIN32
 int _tmain(int argc, _TCHAR* argv[])
@@ -22,15 +20,15 @@ int _tmain(int argc, _TCHAR* argv[])
 int main(int argc, char* argv[])
 #endif
 {
-	vector<string> arguments;
-	for(int i=1; i<argc; i++)
+  vector<string> arguments;
+  for (int i = 1; i < argc; i++)
 #ifdef _WIN32
-		arguments.push_back(ToUtf8(reinterpret_cast<const wchar_t*>(argv[i])));
+    arguments.push_back(ToUtf8(reinterpret_cast<const wchar_t*>(argv[i])));
 #else
-		arguments.push_back(argv[i]);
+    arguments.push_back(argv[i]);
 #endif
 
-	Store store;
+  Store store;
 
-	return store.CommandLine(arguments) ? 0:-1;
+  return store.CommandLine(arguments) ? 0 : -1;
 }
