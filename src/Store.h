436128b03b869859bfd61c9e9689b613aeabf82b
Zlatin Georgiev <32363835+alphasoftwarebg@users.noreply.github.com>
2019-06-09 21:06:33 +0300

    Update Store.h

--- src/Store.h
+++ src/Store.h
@@ -1,47 +1,43 @@
-																			 /*
+/*
 -------- Store.h --------------------------------------------------------------
 
-	ZZZ Base Mini sample warehouse program
+ZZZ Base Mini sample warehouse program
 
 -------- © 2019 ZZZ Ltd. - Bulgaria -------------------------------------------
-																			 */
+*/
 #pragma once
 
 #include <string>
 #include <vector>
 #include "../include/ZZZBaseMini.h"
 
-
 const std::string STORE_BASE = "Store.zzz";
 
+class Store {
+ public:
+  Store();
+  virtual ~Store();
 
-class Store
-{
-public:
-	Store();
-	virtual ~Store();
+  virtual bool Add(std::string const& barcode,
+                   std::string const& name,
+                   std::string const& measure,
+                   std::string const& quantity,
+                   std::string const& price);
 
-	virtual bool Add(
-		std::string const& barcode,
-		std::string const& name,
-		std::string const& measure,
-		std::string const& quantity,
-		std::string const& price);
+  virtual bool Get(std::string const& barcode, std::string const& quantity);
 
-	virtual bool Get(
-		std::string const& barcode,
-		std::string const& quantity);
+  virtual bool Report(std::string barcode);
 
-	virtual bool Report(std::string barcode);
+  bool CommandLine(std::vector<std::string> arguments);
 
-	bool CommandLine(std::vector<std::string> arguments);
+ protected:
+  ZzzBase::ZZZBaseMini* zzz;
+  std::string divider;
 
-protected:
-	ZzzBase::ZZZBaseMini* zzz;
-	std::string divider;
-
-	void Init();
-	void Clean();
-	std::string path(std::string const& dividerPath);
-	void setValueByPath(long long int id, std::string const& dividerPath, double const value);
+  void Init();
+  void Clean();
+  std::string path(std::string const& dividerPath);
+  void setValueByPath(long long int id,
+                      std::string const& dividerPath,
+                      double const value);
 };
